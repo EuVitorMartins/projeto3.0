21f07aaa348125331cfcdc0a27c41a8f524b0408
Vitor Martins <vitor.martins.1321@gmail.com>
2024-01-21 00:02:40 -0300

    Primero commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const div = document.querySelector('.texto-span');
+const textos = ['Web.', 'Front-end.', 'Full Stak.'];
+
+function escrever(str, done) {
+    const char = str.split('').reverse();
+    const typer = setInterval(function() {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 500); 
+        }
+        const next = char.pop();
+        div.innerHTML += next;
+    }, 200);
+}
+
+function limpar(done) {
+    const char = div.innerHTML;
+    let nr = char.length;
+    const typer = setInterval(function() {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 200);
+}
+
+function rodape(conteudos, el) {
+    let atual = -1;
+	function prox(cb){
+		if (atual < conteudos.length - 1) atual++;
+		else atual = 0;
+		let str = conteudos[atual];
+		escrever(str, function(){
+			limpar(prox);
+		});
+	}
+	prox(prox);
+}
+rodape(textos);
