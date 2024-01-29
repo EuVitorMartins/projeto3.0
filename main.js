e501ae77c6ba902139f022f911b2c9d341e0e234
Vitor Martins <vitor.martins.1321@gmail.com>
2024-01-29 14:04:30 -0300

    Atualização

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const textos = ['Web.', 'Front-end.', 'Full Stak.'];
 
 function escrever(str, done) {
     const char = str.split('').reverse();
-    const typer = setInterval(function() {
+    const typer = setInterval(function () {
         if (!char.length) {
             clearInterval(typer);
-            return setTimeout(done, 500); 
+            return setTimeout(done, 500);
         }
         const next = char.pop();
         div.innerHTML += next;
@@ -16,7 +16,7 @@ function escrever(str, done) {
 function limpar(done) {
     const char = div.innerHTML;
     let nr = char.length;
-    const typer = setInterval(function() {
+    const typer = setInterval(function () {
         if (nr-- == 0) {
             clearInterval(typer);
             return done();
@@ -27,14 +27,25 @@ function limpar(done) {
 
 function rodape(conteudos, el) {
     let atual = -1;
-	function prox(cb){
-		if (atual < conteudos.length - 1) atual++;
-		else atual = 0;
-		let str = conteudos[atual];
-		escrever(str, function(){
-			limpar(prox);
-		});
-	}
-	prox(prox);
+    function prox(cb) {
+        if (atual < conteudos.length - 1) atual++;
+        else atual = 0;
+        let str = conteudos[atual];
+        escrever(str, function () {
+            limpar(prox);
+        });
+    }
+    prox(prox);
 }
 rodape(textos);
+
+// CARDS DO SOBRE
+
+const cards = document.querySelectorAll('.card-sobre')
+
+cards.forEach((card) => {
+    card.addEventListener('click', (e) => {
+        card.classList.toggle('flip')
+    })
+})
+
